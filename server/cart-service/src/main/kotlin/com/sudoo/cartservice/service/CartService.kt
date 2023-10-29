3e8d10348fa3fc24393f55f23f35db08142cb175
MinhHD <hdminh5901@gmail.com>
2023-10-29 23:33:04 +0700

    feat: refactor code

--- server/cart-service/src/main/kotlin/com/sudoo/cartservice/service/CartService.kt
+++ server/cart-service/src/main/kotlin/com/sudoo/cartservice/service/CartService.kt
@@ -7,15 +7,31 @@ import com.sudoo.cartservice.controller.dto.UpsertCartProductDto
 
 
 interface CartService {
+
+    //---Cart-----------
     suspend fun createCartByStatus(userId: String, status: String): CartDto
     suspend fun createNewCart(userId: String): CartDto
-    suspend fun updateStatusCart(userId: String): CartDto
     suspend fun getCountItemActiveCart(userId: String): Int
 
-    //--------------
+    //---Active Cart-----------
     suspend fun getActiveCart(userId: String): CartDto
     suspend fun getCartById(userId: String, cartId: String, hasRoute: Boolean): CartDto
     suspend fun getCartProducts(cartId: String): List<CartProductDto>
+
+    //---CartProductInActiveCart-------------
+
+    suspend fun updateProductInActiveCart(
+        userId: String,
+        upsertCartProductDto: UpsertCartProductDto
+    ): CartDto
+
+    suspend fun deleteCartProduct(
+        cartId: String,
+        cartProductId: String,
+    ): Boolean
+
+
+    //---Processing Cart-------------
 
     /**
      * Tạo processing cart từ active cart
@@ -31,15 +47,5 @@ interface CartService {
      * Update active cart after checkout processing cart
      */
     suspend fun checkoutProcessingCart(userId: String)
-    //--------------
 
-    suspend fun updateProductInActiveCart(
-        userId: String,
-        upsertCartProductDto: UpsertCartProductDto
-    ): CartDto
-
-    suspend fun deleteCartProduct(
-        cartId: String,
-        cartProductId: String,
-    ): Boolean
 }
