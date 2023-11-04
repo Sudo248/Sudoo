fee318ad28dc581a2798e38f3d7f891d22a56f57
MinhHD <hdminh5901@gmail.com>
2023-11-04 16:22:31 +0700

    fix: payment_service to order_service

--- server/api-gateway/src/main/kotlin/com/sudoo/apigateway/config/SudooServices.kt
+++ server/api-gateway/src/main/kotlin/com/sudoo/apigateway/config/SudooServices.kt
@@ -6,7 +6,7 @@ enum class SudooServices(val id: String, val uri: String, val pattern: Array<Str
     STORAGE_SERVICE("STORAGE-SERVICE", "lb://STORAGE-SERVICE", arrayOf("/api/v1/storage/**")),
     USER_SERVICE("USER-SERVICE", "lb://USER-SERVICE", arrayOf("/api/v1/users/**", "/api/v1/addresses/**")),
     CART_SERVICE("CART-SERVICE", "lb://CART-SERVICE", arrayOf("/api/v1/carts/**")),
-    PAYMENT_SERVICE("PAYMENT-SERVICE", "lb://PAYMENT-SERVICE", arrayOf("/api/v1/payment/**", "/payment/**")),
-    ORDER_SERVICE("ORDER-SERVICE", "lb://ORDER-SERVICE", arrayOf("/api/v1/orders/**")),
+//    PAYMENT_SERVICE("PAYMENT-SERVICE", "lb://PAYMENT-SERVICE", arrayOf("/api/v1/payment/**", "/payment/**")),
+    ORDER_SERVICE("ORDER-SERVICE", "lb://ORDER-SERVICE", arrayOf("/api/v1/orders/**","/api/v1/payment/**", "/payment/**")),
     NOTIFICATION_SERVICE("NOTIFICATION-SERVICE", "lb://NOTIFICATION-SERVICE", arrayOf("/api/v1/notifications/**", "/api/v1/chats/**")),
 }
