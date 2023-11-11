bf6fb9a12403cfc3b8848c397ad60b9df183f47f
DuongLH <duong.lh@teko.vn>
2023-11-11 23:19:54 +0700

    [BE] compress database.zip

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,22 +1,22 @@
 
 # download web from git
-cd /
+cd /home
 mkdir -p Sudoo
 cd Sudoo
 
-curl --request GET -sL \
-     --url 'https://raw.githubusercontent.com/Sudo248/Sudoo/be-dev/client/web/sudoo/web.zip'\
-     --output './'
+wget https://storage.googleapis.com/sudoo-buckets/web-storage/web.zip
+
+wget
 
 unzip web.zip
 
 rm web.zip
 
 # run nginx
-docker run -d -p 80:80 -v /Sudoo/web:/app/web --restart always --name sudoo-nginx sudo248dev/sudoo-nginx
+docker run -d -p 80:80 -v /home/Sudoo/web:/app/web --restart always --name sudoo-nginx sudo248dev/sudoo-nginx
 
-curl --request GET -sL \
-     --url 'https://raw.githubusercontent.com/Sudo248/Sudoo/be-dev/server/docker-compose.yml'\
-     --output '/Sudoo/docker-compose.yml'
+wget https://raw.githubusercontent.com/Sudo248/Sudoo/be-dev/server/docker-compose.yml
 
-docker-compose up -d
+wget
+
+docker compose up -d
