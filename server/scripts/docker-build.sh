f08167c8c21e04027ec81f130b3dc67211be5e34
DuongLH <duong.lh@teko.vn>
2023-11-12 00:12:00 +0700

    [BE] update application.property to deploy

--- server/scripts/docker-build.sh
+++ server/scripts/docker-build.sh
@@ -9,20 +9,19 @@ strings=(
   storage-service
 #  sudoo-notification-service
 )
-admin="sudo248dev/"
+admin="sudo248dev"
 # shellcheck disable=SC2046
 docker rm -f $(docker container ps -a -q)
 
 for i in "${strings[@]}"; do
-  docker rmi "$admin$i"
+  docker rmi "${admin}/sudoo-${i}"
 done
 
 count=0
 for i in "${strings[@]}"; do
-  image="${admin}sudoo-$i"
+  image="${admin}/sudoo-$i"
   echo "Build docker image: $i context: $i"
   docker build -t "${image}" "../${i}"
   ((count=count+1))
 done
 echo "Total build $count docker image"
-#docker network create --subnet 172.18.0.0/16 --gateway 172.18.0.1 sudoo-network
