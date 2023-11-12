93c4330946b5e53527d98d9a54d7f231c053320f
DuongLH <duong.lh@teko.vn>
2023-11-12 22:29:45 +0700

    [BE] fix request mapping banner

--- server/product-service/src/main/kotlin/com/sudoo/productservice/controller/BannerController.kt
+++ server/product-service/src/main/kotlin/com/sudoo/productservice/controller/BannerController.kt
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/discovery/banners")
+@RestController
+@RequestMapping("/discovery/banners")
 class BannerController(
     private val imageService: ImageService
 ) : BaseController() {
