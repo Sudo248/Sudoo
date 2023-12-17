cd434bae8198cecb75221a9553a55cb3ef069196
DuongLH <duong.lh@teko.vn>
2023-12-17 16:03:34 +0700

    [AI] update prediction

---
+++ recommend/server/services/test.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+# m = {}
+# for i in range(10):
+#     m[i] = f"index {i}"
+# a = np.array([10,5,6,7,3,2,1])
+# b = a.argsort()
+# def f(x):
+#     return m[x]
+# print(b)
+# print(list(map(f, b)))
+
+# id = [0,1,2,3,4,5]
+
+# category = ["c3", "c5", "c2", "c3", "c4", "c5"]
+
+# product = ["p0", "p1", "p2", "p3", "p4", "p5"]
+
+# a = pd.DataFrame({'id': id[::-1], 'category': category})
+
+# b = pd.DataFrame({'id': id, 'product': product})
+
+# c = a.merge(b, how='left')
+
+# print(a)
+# print(b)
+# print(c)
+
+# a = pd.DataFrame()
+
+# a = a._append({'id': int(0), 'info': [0,1,0,1]}, ignore_index = True)
+
+# a = a._append({'id': int(1), 'info': [1,0,0,1]}, ignore_index = True)
+
+# print(a)
+
+a = [i for i in range(20)]
+print(a)
+print(a[0:10]) 
+print(a[10:20])
