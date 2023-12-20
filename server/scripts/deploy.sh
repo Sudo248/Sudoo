9542ea6e498108ea63957b7ce7c63c8f666987bc
DuongLH <duong.lh@teko.vn>
2023-12-21 02:26:06 +0700

    [BE] fix docker-compose

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -6,7 +6,6 @@ cd Sudoo
 
 wget https://raw.githubusercontent.com/Sudo248/Sudoo/web-dev/client/web/sudoo/deploy/web.zip
 wget https://raw.githubusercontent.com/Sudo248/Sudoo/be-dev/server/database.zip
-wget https://raw.githubusercontent.com/Sudo248/Sudoo/be-dev/server/scripts/docker-pull.sh
 wget https://raw.githubusercontent.com/Sudo248/Sudoo/be-dev/server/scripts/docker-remove.sh
 wget https://raw.githubusercontent.com/Sudo248/Sudoo/be-dev/server/nginx/conf/sudoo.conf
 
@@ -20,18 +19,11 @@ cp sudoo.conf /etc/nginx/conf.d
 
 systemctl restart nginx
 
-#pull all image
-chmod +x ./docker-pull.sh
-./docker-pull.sh
-
-# run nginx
-#docker run -d -p 80:80 -v /home/Sudoo/web:/app/web --add-host=host.docker.internal:host-gateway --name sudoo-nginx sudo248dev/sudoo-nginx
-
 wget https://raw.githubusercontent.com/Sudo248/Sudoo/be-dev/server/docker-compose.yml
 
-docker network create --subnet 172.18.0.0/16 --gateway 172.18.0.1 sudoo-network
+#docker network create --subnet 172.18.0.0/16 --gateway 172.18.0.1 sudoo-network
 
-sudo docker network create --driver=overlay --attachable --subnet 172.18.0.0/16 --gateway 172.18.0.1 sudoo-network
+docker network create --driver=overlay --attachable --subnet 172.18.0.0/16 --gateway 172.18.0.1 sudoo-network
 
 mkdir mysql
 
