5aade3ae0eb24dc1fabec3696721a419bfef54ab
Sudo <Sudo248.dev@gmail.com>
2023-12-30 10:42:10 +0700

    [WEB] add role when regiser

--- client/web/sudoo/lib/data/api/auth/request/account_request.dart
+++ client/web/sudoo/lib/data/api/auth/request/account_request.dart
@@ -2,17 +2,21 @@
 
 import 'package:sudoo/data/api/base_request.dart';
 
+import '../../../../domain/model/auth/role.dart';
+
 class AccountRequest extends BaseRequest {
   final String emailOrPhoneNumber;
   final String password;
+  final Role role;
 
-  AccountRequest({required this.emailOrPhoneNumber, required this.password});
+  AccountRequest({required this.emailOrPhoneNumber, required this.password, this.role = Role.STAFF});
 
   @override
   Map<String, dynamic> toJson() {
     return {
       "emailOrPhoneNumber": emailOrPhoneNumber,
       "password": password,
+      "role": role.value,
     };
   }
 }
